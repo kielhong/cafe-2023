59d64062b84b88cd2376f63e3e64e3d584228075
mh.hong <kielhong@gmail.com>
2023-03-18 21:59:29 +0900

    chore: lint 오류 수정

--- src/test/kotlin/com/widehouse/cafe/article/ArticleServiceTest.kt
+++ src/test/kotlin/com/widehouse/cafe/article/ArticleServiceTest.kt
@@ -9,7 +9,6 @@ import com.widehouse.cafe.common.sequence.SequenceService
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
