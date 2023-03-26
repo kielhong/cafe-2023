fdd789cf58ab2fb319e0d4ab506e5375d26c2c83
mh.hong <kielhong@gmail.com>
2023-03-26 10:39:01 +0900

    fix : lint 오류 수정

--- src/main/kotlin/com/widehouse/cafe/cafe/model/CategoryRepository.kt
+++ src/main/kotlin/com/widehouse/cafe/cafe/model/CategoryRepository.kt
@@ -1,6 +1,5 @@
 package com.widehouse.cafe.cafe.model
 
-import com.widehouse.cafe.cafe.model.Category
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
 interface CategoryRepository : ReactiveMongoRepository<Category, Long>
